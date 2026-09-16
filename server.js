14af90e020d0f65bdb1604e9202a4e1dc4cf810a
Kapeel Kumar <editsvideo251@gmail.com>
2026-09-16 19:40:44 +0500

    Fix static file serving on Vercel

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
